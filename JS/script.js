787fa2e213cd54ad261d5fd292b006cc33fa43ca
Teri11 <augustineteri157@gmail.com>
2022-01-14 19:25:49 +0300

    added opening alert

---
+++ JS/script.js
@@ -0,0 +1,2 @@
+let restaurant=confirm("We'd like to show you notifications for the latest pizza offers and updates.")
+alert("Thank you for your response\n we are here to serve you better\n and welcome again.");
