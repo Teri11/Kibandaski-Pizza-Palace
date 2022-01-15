76692cd22053a012e1a38a94e693685c0c9c6953
Teri11 <augustineteri157@gmail.com>
2022-01-15 08:27:57 +0300

    added hover option for landing image page

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,12 @@
+// alert news...
 let restaurant=confirm("We'd like to show you notifications for the latest pizza offers and updates.")
 alert("Thank you for your response\n we are here to serve you better\n and welcome again.");
+
+// hover on landing page image..
+$(document).ready(function() {
+  $("#work1").hover(function(){
+  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
+   }, function(){
+  $(".work" ,this).slideToggle(100, "linear").display(100, "linear");
+  });
+});
